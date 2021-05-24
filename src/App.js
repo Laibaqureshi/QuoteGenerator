537c489dd8424ac44a5623845a6b3ffa2bfb2337
Laibaqureshi <laibajmi227@gmail.com>
2021-05-24 13:27:17 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Quote from "./Quote";
+// import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 function App() {
+  const [currQuote, setCurrQuote] = useState(null);
+  const url = "https://type.fit/api/quotes";
+  const [apiQuotes, setApiQuotes] = useState([]);
+  //  const [author,setAuthor]=useState(null);
+
+  const change = () => {
+    setCurrQuote(apiQuotes[Math.floor(Math.random() * 1643)]);
+  };
+  const open = () => {
+    console.log(currQuote.text);
+    console.log(currQuote.author);
+
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${currQuote.text} - ${currQuote.author}`;
+    console.log(twitterUrl);
+    console.log("hi");
+    window.open(twitterUrl, "_blank");
+  };
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      //  console.log(response);
+      // console.log(response.data[99].text);
+      setApiQuotes(response.data);
+
+      setCurrQuote(response.data[Math.floor(Math.random() * 1643)]);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quoteWrapper">
+      <Quote currQuote={currQuote} change={change} open={open} />
     </div>
   );
 }
-
 export default App;
---
+++ src/Quote.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./quote.css";
+const Quote = ({ currQuote, change, open }) => {
+  return (
+    <div className="quote-container" id="quote-container">
+      <div className="quote-text">
+        <i className="fas fa-quote-left"></i>
+
+        <span id="quote">"{currQuote ? currQuote.text : ""}</span>
+      </div>
+      <div className="quote-author">
+        <span id="author">
+          {currQuote ? (currQuote.author ? currQuote.author : "Anonymous") : ""}
+        </span>
+      </div>
+      <div className="button-container">
+        <button
+          className="twitter-button"
+          id="twitter"
+          title="Tweet This!"
+          onClick={open}
+        >
+          <i className="fab fa-twitter"></i>
+          Tweet
+        </button>
+        <button id="new-quote" onClick={change}>
+          New Quote
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Quote;
